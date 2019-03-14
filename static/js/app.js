e682f990eacc20ea1b005c2284c10acb6203e553
jormax <43761101+jormax14@users.noreply.github.com>
2019-03-14 16:33:08 -0500

    appended table and rows with data to webpage

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,19 @@
-// from data.js
 var tableData = data;
 
+var tbody = d3.select('tbody');
 
+console.log(tableData);
+
+tableData.forEach((ufoReport) => {
+
+    // console.log(ufoReport)
+    
+    var row = tbody.append('tr');
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        
+        // console.log(key, value);
+    
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
