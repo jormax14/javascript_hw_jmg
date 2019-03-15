40df83016c1ce86a54b5cd2535fea5abd8295ab1
jormax <43761101+jormax14@users.noreply.github.com>
2019-03-14 20:46:28 -0500

    fixed the date form

--- static/js/app.js
+++ static/js/app.js
@@ -2,18 +2,39 @@ var tableData = data;
 
 var tbody = d3.select('tbody');
 
-console.log(tableData);
+function init() {
 
-tableData.forEach((ufoReport) => {
-
-    // console.log(ufoReport)
-    
-    var row = tbody.append('tr');
-    Object.entries(ufoReport).forEach(([key, value]) => {
-        
-        // console.log(key, value);
-    
-        var cell = row.append('td');
-        cell.text(value);
+    tableData.forEach((ufoReport) => {
+        var row = tbody.append('tr');
+        Object.entries(ufoReport).forEach(([key, value]) => {  
+            var cell = row.append('td');
+         cell.text(value);
+        });
     });
-});
+}
+
+
+function handleSubmit() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select('#datetime');
+
+    var inputValue = inputElement.property('value');
+
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    tbody.selectAll('tr').remove();
+
+    filteredData.forEach((ufoRecord) => {
+        var row = tbody.append('tr');
+        Object.entries(ufoRecord).forEach(([key, value]) => {
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+}
+
+init();
+
+d3.select('#filter-btn').on('click', handleSubmit);
